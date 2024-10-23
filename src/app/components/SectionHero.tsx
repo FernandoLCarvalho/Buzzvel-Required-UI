ebf4d775d1c809ce4c7a0ca43f3a050ec00ea03c
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-10-23 03:49:26 -0300

    adjusted css issue for mobile at header and sectionHero

--- src/app/components/SectionHero.tsx
+++ src/app/components/SectionHero.tsx
@@ -5,15 +5,17 @@ import { FiArrowRight } from 'react-icons/fi';
 
 export default function SectionHero() {
     return (
-        <main> {/* This section looks like the main content to me */}
+        <main> {/* This section looks like the main content to me */ }
             <section className={styles.heroSection}>
-                <div className="grid" style={{ margin: '0' }}>
+                <div className="grid" style={{margin: '0'}}>
                     <div className={`col-12 sm:col-6 ${styles.heroTextContainer}`}>
                         <h1 className={styles.heroTitle}>Get the Sun to Power Your Home</h1>
                         <p className={styles.heroSubtitle}>Viverra viverra nibh enim et aliquam, enim. Tempor, sit mus viverra orci dui consequat turpis scelerisque.</p>
-                        <button className={styles.quoteButton}>
-                            Request a Quote <FiArrowRight className={styles.arrowIcon} />
-                        </button>
+                        <div className={`${styles.quoteButtonContainer} ${styles.mobileCenter}`}>
+                            <button className={styles.quoteButton}>
+                                Request a Quote <FiArrowRight className={styles.arrowIcon} />
+                            </button>
+                        </div>
                     </div>
                     <div className={`col-12 ${styles.heroQuoteContainer}`}>
                         <p className={styles.heroQuote}>“Viverra viverra nibh enim et aliquam, enim. Tempor, sit mus viverra orci dui consequat turpis scelerisque faucibus.”</p>
