9b343b4b518d08153585ef0d1ec360d240f745bb
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-10-24 01:48:50 -0300

    sectionPickSun implemented

--- src/app/components/SectionPickSun.tsx
+++ src/app/components/SectionPickSun.tsx
@@ -4,8 +4,10 @@ import styles from '../styles/sectionPickSun.module.css';
 export default function SectionPickSun() {
     return (
         <section className={styles.pickSunSection} aria-labelledby="pick-sun-title">
-            <div className={styles.backgroundContainer}>
-                <img src='/Ellipse 14.svg' alt='Ellipse background' className={styles.ellipseBackground} />
+            <div className={`grid ${styles.backgroundContainer}`}>
+                <img src='/Ellipse 14.svg' alt='Ellipse background left' className={styles.ellipseBackgroundLeft} />
+                <img src='/Ellipse 15.svg' alt='Ellipse background right' className={styles.ellipseBackgroundRight} />
+            <div className={`col-6 ${styles.desktopImage}`}/>
             </div>
             <h2 id="pick-sun-title" className={styles.sectionCaption}>No more waste</h2>
             <h1 className={styles.sectionTitle}>Pick the Sun</h1>
