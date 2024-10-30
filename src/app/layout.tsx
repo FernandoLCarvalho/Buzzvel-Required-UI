c64e983881960d4997cd78c7fc2328e24cef73d3
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-10-30 09:23:50 -0300

    added the vercel url to metatags in Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Viewport } from 'next/dist/lib/metadata/types/extra-types';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://seu-dominio.com'),
+  metadataBase: new URL('https://buzzvel-ui-reproduction-83cyyo8d2.vercel.app/'),
   title: 'Buzzvel Project',
   description: 'Buzzvel 2024 Dev Team Test',
   keywords: 'solar energy, home power, renewable energy, solar panels, soller, sustainable energy',
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
     description: 'Discover Soller’s solutions for sustainable and efficient solar energy to power your home.',
     images: '/Mask masked.svg',
     type: 'website',
-    url: 'https://seu-dominio.com',
+    url: 'https://buzzvel-ui-reproduction-83cyyo8d2.vercel.app/',
   },
   twitter: {
     card: 'summary_large_image',
