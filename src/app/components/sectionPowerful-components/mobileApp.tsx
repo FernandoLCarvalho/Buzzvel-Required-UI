c4e1a232d6a27b909ab8b24cbea2084cc1d07ae1
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2024-10-31 00:38:21 -0300

    added lazy loading to mobile app miniatures

--- src/app/components/sectionPowerful-components/mobileApp.tsx
+++ src/app/components/sectionPowerful-components/mobileApp.tsx
@@ -8,15 +8,15 @@ export default function MobileApp2() {
             <div className={`grid ${styles.mobileAppContent}`}>
                 {/* Top Image Section */}
                 <div className={`col-12 ${styles.firstImage}`} style={{ padding: 0 }}>
-                    <Image src='/Image 1 Mobile App 2.svg' alt="Top Section Image" width="344" height="236" className={styles.firstImageContent}/>
+                    <Image src='/Image 1 Mobile App 2.svg' alt="Top Section Image" width="344" height="236" className={styles.firstImageContent} loading='lazy'/>
                 </div>
 
                 {/* Middle Content Section */}
                 <div className={`col-12 ${styles.secondImage}`} style={{ padding: 0 }}>
-                    <Image src='/Image 2 Mobile App 2.png' alt="Middle Content" width="320" height="100" className={styles.secondImageContent} />
+                    <Image src='/Image 2 Mobile App 2.png' alt="Middle Content" width="320" height="100" className={styles.secondImageContent} loading='lazy'/>
                 </div>
                 <div className={`col-12 ${styles.thirdImage}`} style={{ padding: 0, marginBottom: '2%' }}>
-                    <Image src='/Image 3 Mobile App 2.png' alt="Bottom Content" width={180} height={19.2} className={styles.thirdImageContent}/>
+                    <Image src='/Image 3 Mobile App 2.png' alt="Bottom Content" width={180} height={19.2} className={styles.thirdImageContent} loading='lazy'/>
                 </div>
 
                 {/* Bottom Circles Section */}
@@ -31,13 +31,13 @@ export default function MobileApp2() {
             {/* Bottom Menu */}
             <div className={`grid ${styles.bottomMenu}`} style={{ margin: '0' }}>
                 <div className='col-4' style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Image src='/pulse-outline.svg' alt="Pulse" className={styles.bottomMenuIcon} width="50" height="31" />
+                    <Image src='/pulse-outline.svg' alt="Pulse" className={styles.bottomMenuIcon} width="50" height="31" loading='lazy'/>
                 </div>
                 <div className='col-4' style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Image src='/search-outline.svg' alt="Search" className={styles.bottomMenuIcon} width="50" height="31" />
+                    <Image src='/search-outline.svg' alt="Search" className={styles.bottomMenuIcon} width="50" height="31" loading='lazy' />
                 </div>
                 <div className='col-4' style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Image src='/menu-outline.svg' alt="Menu" className={styles.bottomMenuIcon} width="50" height="31" />
+                    <Image src='/menu-outline.svg' alt="Menu" className={styles.bottomMenuIcon} width="50" height="31" loading='lazy'/>
                 </div>
             </div>
 
--- src/app/components/sectionServices-components/mobileApp.tsx
+++ src/app/components/sectionServices-components/mobileApp.tsx
@@ -7,18 +7,18 @@ export default function MobileApp2() {
             
                 <div className={`grid ${styles.mobileAppContent}`} style={{margin: 0}}>
                     <div className='col-8' style={{ padding: 0, display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-                        <Image src='/Mobile App Left Arrow.png' alt="Left Arrow" width={57.6} height={57.6} className={styles.arr}/>
+                        <Image src='/Mobile App Left Arrow.png' alt="Left Arrow" width={57.6} height={57.6} className={styles.arr} loading='lazy'/>
                         <div className={`${styles.title}`} />
                     </div>
 
                     <div className={`col-9 ${styles.description}`} style={{padding: 0}}>
-                        <Image src='/Mobile App Description.svg' alt="Descrição" width={268.8} height={48} />
+                        <Image src='/Mobile App Description.svg' alt="Descrição" width={268.8} height={48} loading='lazy'/>
                     </div>
                     <div className={`${styles.featureCardsContainer}`}>
                    
                         <div className={styles.featureCard}>
-                            <Image src='/Ellipse 3.svg' alt="Ellipse 3" className={`${styles.ellipse} ${styles.ellipse3}`} width="97" height="91"/>
-                            <Image src='/Ellipse 5 (Stroke).svg' alt="Ellipse 5" className={`${styles.ellipse} ${styles.ellipse5}`} width="66" height="66" />
+                            <Image src='/Ellipse 3.svg' alt="Ellipse 3" className={`${styles.ellipse} ${styles.ellipse3}`} width="97" height="91" loading='lazy'/>
+                            <Image src='/Ellipse 5 (Stroke).svg' alt="Ellipse 5" className={`${styles.ellipse} ${styles.ellipse5}`} width="66" height="66" loading='lazy'/>
                         </div>
 
                         <div className={styles.featureCard}>
@@ -28,8 +28,8 @@ export default function MobileApp2() {
                         </div>
 
                         <div className={`${styles.featureCard} ${styles.lastFeatureCard}`}>
-                            <Image src='/Star 1 (Stroke).svg' alt="Star 1" className={`${styles.star}`} width="92" height="88"/>
-                            <Image src='/Ellipse 5 (Stroke).svg' alt="Circle" className={`${styles.ellipse} ${styles.ellipseForStar}`} width="66" height="66"/>
+                            <Image src='/Star 1 (Stroke).svg' alt="Star 1" className={`${styles.star}`} width="92" height="88" loading='lazy'/>
+                            <Image src='/Ellipse 5 (Stroke).svg' alt="Circle" className={`${styles.ellipse} ${styles.ellipseForStar}`} width="66" height="66" loading='lazy'/>
                         </div>
                     </div>
 
@@ -46,13 +46,13 @@ export default function MobileApp2() {
                 </div>
                 <div className={`grid ${styles.bottomMenu}`} style={{margin:'0'}}>
                     <div className='col-4' style={{ display: 'flex', justifyContent: 'center' }}>
-                        <Image src='/pulse-outline.svg' alt="Pulse" className={styles.bottomMenuIcon} width="50" height="31"/>
+                        <Image src='/pulse-outline.svg' alt="Pulse" className={styles.bottomMenuIcon} width="50" height="31" loading='lazy'/>
                     </div>
                     <div className='col-4' style={{ display: 'flex', justifyContent: 'center' }}>
-                        <Image src='/search-outline.svg' alt="Search" className={styles.bottomMenuIcon} width="50" height="31"/>
+                        <Image src='/search-outline.svg' alt="Search" className={styles.bottomMenuIcon} width="50" height="31" loading='lazy'/>
                     </div>
                     <div className='col-4' style={{ display: 'flex', justifyContent: 'center' }}>
-                        <Image src='/menu-outline.svg' alt="Menu" className={styles.bottomMenuIcon} width="50" height="31"/>
+                        <Image src='/menu-outline.svg' alt="Menu" className={styles.bottomMenuIcon} width="50" height="31" loading='lazy'/>
                     </div>
                 </div>
         
